c3042ef7bd718860113beb6e14cb883f0711fa17
Łukasz Michalak <lupinummail@gmail.com>
2025-09-06 22:20:35 +0200

    Initial Commit

---
+++ NineMen'sMorris/NineMen_sMorrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  NineMen_sMorrisApp.swift
+//  NineMen'sMorris
+//
+//  Created by Łukasz Michalak on 06/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct NineMen_sMorrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
