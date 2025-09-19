b91676c16cb7593c43766f2cbe0e039325b95562
Łukasz Michalak <lupinummail@gmail.com>
2025-09-19 23:13:16 +0200

    Displaying board and points

---
+++ NineMen'sMorris/Game/Point.swift
@@ -0,0 +1,18 @@
+//
+//  Point.swift
+//  NineMen'sMorris
+//
+//  Created by Łukasz Michalak on 19/09/2025.
+//
+
+import SwiftUI
+
+struct Point: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Point()
+}
